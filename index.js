fd4b4ea07893ef49f3328569dd326305c1be2630
admin <khadijaaws21@gmail.com>
2024-07-22 16:13:14 +0200

    hello

---
+++ index.js
@@ -0,0 +1,19 @@
+;(function (exports) {
+  function greet(name) {
+    const options = {
+      second: 'numeric',
+      minute: 'numeric',
+      hour: 'numeric',
+      day: 'long',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'numeric',
+    }
+
+    const now = new Date()
+    const formattedDate = now.toLocaleDateString('en-US', options)
+    return `Hello, ${name}! Today is ${formattedDate}.`
+  }
+
+  exports.greet = greet
+})(this)
